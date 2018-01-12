86141c13250ee4c7de3d64d621c0fd8f1302ef9e
Mathieu <mathieu.ouvrard.ext@mappy.com>
2018-01-12 11:24:23 +0100

    IOMA-7543: add script to sync Mappy fork with Mapbox repo

---
+++ scripts/sync_mappy_fork.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# sync_mappy_fork - A script to sync Mappy fork of MbNavigation with Mapbox repository
+
+git remote -v | grep 'upstream' &> /dev/null || git remote add upstream https://github.com/mapbox/mapbox-navigation-ios.git
+git remote update
+git checkout master
+git pull origin master
+git merge upstream/master
+git push origin master
+
